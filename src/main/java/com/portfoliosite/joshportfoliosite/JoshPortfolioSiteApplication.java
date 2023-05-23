d6f6c8b2160bb0013b35e9e2cb60f439ab062515
Josh Guevarra <jguevarra2@dons.usfca.edu>
2023-05-23 14:21:19 -0700

    Added Spring Boot files

---
+++ src/main/java/com/portfoliosite/joshportfoliosite/JoshPortfolioSiteApplication.java
@@ -0,0 +1,13 @@
+package com.portfoliosite.joshportfoliosite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoshPortfolioSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoshPortfolioSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfoliosite/joshportfoliosite/JoshPortfolioSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfoliosite.joshportfoliosite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoshPortfolioSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
