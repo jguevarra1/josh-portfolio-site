0cd060a41b0fdbc1cc84b7b1a9922c3cf04373d2
Josh Guevarra <jguevarra2@dons.usfca.edu>
2023-05-24 16:33:29 -0700

    Fixed paths of CSS and images

--- src/main/java/com/portfoliosite/joshportfoliosite/Ticket/TicketController.java
+++ src/main/java/com/portfoliosite/joshportfoliosite/Ticket/TicketController.java
@@ -2,8 +2,12 @@ package com.portfoliosite.joshportfoliosite.Ticket;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping(value = "/")
 public class TicketController
 {
     // TODO set up the controller code
@@ -15,4 +19,6 @@ public class TicketController
     {
         this.ticketService = ticketService;
     }
+
+
 }
